a9be4421afa95eb7634277ad5394248f122e7b0f
Akilgour <kilgourkilgour@hotmail.com>
2015-10-22 12:36:03 +0100

    Added CharCount to TextAnalytics

--- TextFileReader/TextAnalytics.cs
+++ TextFileReader/TextAnalytics.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TextFileReader
@@ -9,10 +10,18 @@ namespace TextFileReader
     public class TextAnalytics
     {
         public string OriginalText { get; private set; }
+        public string TextWithTabAndSpaceRemoved { get; private set; }
 
         public TextAnalytics(string text)
         {
             OriginalText = text;
+        }
+
+        public int CharCount()
+        {
+            //This regex will strip out all tabs and spaces.
+            string TextWithTabAndSpaceRemoved = Regex.Replace(OriginalText, @"\t| ", "");
+            return TextWithTabAndSpaceRemoved.Length;
         }
 
     }
--- UnitTestProject/TextAnalytics_CharCount.cs
+++ UnitTestProject/TextAnalytics_CharCount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextFileReader;
 
 namespace UnitTestProject
 {
@@ -7,9 +8,73 @@ namespace UnitTestProject
     public class TextAnalytics_CharCount
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ReadThreeChar()
         {
+            //Arrange 
+            var value = "ABC";
+            var textAnalytics = new TextAnalytics(value);
 
+            //Act 
+            var charCount = textAnalytics.CharCount();
+
+            //Assert
+            Assert.AreEqual(3, charCount);
+        }
+
+        [TestMethod]
+        public void ReadThreeCharWithSpaces()
+        {
+            //Arrange 
+            var value = "A B C ";
+            var textAnalytics = new TextAnalytics(value);
+
+            //Act 
+            var charCount = textAnalytics.CharCount();
+
+            //Assert
+            Assert.AreEqual(3, charCount);
+        }
+
+        [TestMethod]
+        public void ReadThreeCharWithTabAtStart()
+        {
+            //Arrange 
+            var value = "\tABC";
+            var textAnalytics = new TextAnalytics(value);
+
+            //Act 
+            var charCount = textAnalytics.CharCount();
+
+            //Assert
+            Assert.AreEqual(3, charCount);
+        }
+
+        [TestMethod]
+        public void ReadThreeCharWithTabAtMiddle()
+        {
+            //Arrange 
+            var value = "A\tBC";
+            var textAnalytics = new TextAnalytics(value);
+
+            //Act 
+            var charCount = textAnalytics.CharCount();
+
+            //Assert
+            Assert.AreEqual(3, charCount);
+        }
+
+        [TestMethod]
+        public void ReadThreeCharWithTabAtEnd()
+        {
+            //Arrange 
+            var value = "ABC\t";
+            var textAnalytics = new TextAnalytics(value);
+
+            //Act 
+            var charCount = textAnalytics.CharCount();
+
+            //Assert
+            Assert.AreEqual(3, charCount);
         }
     }
 }

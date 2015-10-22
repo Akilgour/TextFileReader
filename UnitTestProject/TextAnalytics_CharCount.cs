ebde831efc8fa3597f4e6226717098eb3cc91779
Akilgour <kilgourkilgour@hotmail.com>
2015-10-22 12:13:02 +0100

    Added test project and first test, first pass

---
+++ UnitTestProject/TextAnalytics_CharCount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TextAnalytics_CharCount
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}

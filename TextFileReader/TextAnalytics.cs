7d6f5cbfee88a68a5ec733d2c371b4996827094c
Akilgour <kilgourkilgour@hotmail.com>
2015-10-22 13:01:34 +0100

    First pass with TenMostFrequentChar plus some unit tests

--- TextFileReader/TextAnalytics.cs
+++ TextFileReader/TextAnalytics.cs
@@ -10,19 +10,36 @@ namespace TextFileReader
     public class TextAnalytics
     {
         public string OriginalText { get; private set; }
-        public string TextWithTabAndSpaceRemoved { get; private set; }
+        public string TextWithoutTabAndSpaceRemoved { get; private set; }
 
         public TextAnalytics(string text)
         {
             OriginalText = text;
+            //This regex will strip out all tabs and spaces.
+            TextWithoutTabAndSpaceRemoved = Regex.Replace(OriginalText, @"\t| ", "");
         }
 
         public int CharCount()
         {
-            //This regex will strip out all tabs and spaces.
-            string TextWithTabAndSpaceRemoved = Regex.Replace(OriginalText, @"\t| ", "");
-            return TextWithTabAndSpaceRemoved.Length;
+            return TextWithoutTabAndSpaceRemoved.Length;
         }
 
+        public Dictionary<char, int> TenMostFrequentChar()
+        {
+            var charDictionary = new  Dictionary<char, int>();
+
+            foreach (var x in TextWithoutTabAndSpaceRemoved)
+            {
+                if (charDictionary.ContainsKey(x))
+                {
+                    charDictionary[x] = charDictionary[x]+1;
+                }
+                else
+                {
+                    charDictionary.Add(x, 1);
+                }
+            }
+            return charDictionary;
+        }
     }
 }
---
+++ UnitTestProject/TextAnalytics_TenMostFrequentChar.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TextFileReader;
+
+namespace UnitTestProject
+{
+    [TestClass]
+   public class TextAnalytics_TenMostFrequentChar
+    {
+
+        [TestMethod]
+        public void ReadThreeChar()
+        {
+            //Arrange 
+            var value = "ABC";
+            var textAnalytics = new TextAnalytics(value);
+
+            //Act 
+            var charDictionary = textAnalytics.TenMostFrequentChar();
+
+            //Assert
+            Assert.AreEqual(3, charDictionary.Count);
+            Assert.AreEqual(1, charDictionary['A']);
+            Assert.AreEqual(1, charDictionary['B']);
+            Assert.AreEqual(1, charDictionary['C']);
+        }
+
+        [TestMethod]
+        public void ReadSameThreeChar()
+        {
+            //Arrange 
+            var value = "AAA";
+            var textAnalytics = new TextAnalytics(value);
+
+            //Act 
+            var charDictionary = textAnalytics.TenMostFrequentChar();
+
+            //Assert
+            Assert.AreEqual(1, charDictionary.Count);
+            Assert.AreEqual(3, charDictionary['A']);
+        }
+
+        [TestMethod]
+        public void ReadThreeWithDuplicateChar()
+        {
+            //Arrange 
+            var value = "AAAABBBCC";
+            var textAnalytics = new TextAnalytics(value);
+
+            //Act 
+            var charDictionary = textAnalytics.TenMostFrequentChar();
+
+            //Assert
+            Assert.AreEqual(3, charDictionary.Count);
+            Assert.AreEqual(4, charDictionary['A']);
+            Assert.AreEqual(3, charDictionary['B']);
+            Assert.AreEqual(2, charDictionary['C']);
+        }
+
+    }
+}
